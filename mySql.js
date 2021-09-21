0b82671400f41d8be35f9dc7589f5efbe326edbe
Md Khalid Hossain <zikaiwen02@gmail.com>
2021-09-21 23:15:40 +0800

    update done

--- mySql.js
+++ mySql.js
@@ -31,7 +31,6 @@ app.post('/student/api/insert_student_info/', (req, res) => {
     const sqlInsert = "insert into students (first_name , last_name , year, phone_number) values (?, ?, ? , ? )";
     myStudentDb.query(sqlInsert, [FName, lName, year, phn], (err, result) => {
         res.send(result)
-        console.log(res, err);
     })
 
 })
@@ -52,10 +51,24 @@ app.delete('/delete/studentinfo/:id', (req, res) => {
     const sqlStatement = "delete from students where id = ?";
     myStudentDb.query(sqlStatement, Studentid, (err, result) => {
         res.send(result)
-        console.log(err, result);
     })
 })
 
+// update data basis on id
+app.put('/update/studentinfo/', (req, res) => {
+    const updateRequestId = req.body.id;
+    const first_name = req.body.firstName;
+    const last_name = req.body.lastName;
+    const year = req.body.year;
+    const phone_number = req.body.phoneNumber;
+
+    const sqlUpdateStudentInfo = "UPDATE students SET first_name =?, last_name = ?, year = ?, phone_number = ? WHERE id = ?";
+    myStudentDb.query(sqlUpdateStudentInfo, [first_name, last_name, year, phone_number, updateRequestId], (err, result) => {
+        console.log(result);
+    })
+    console.log(updateRequestId, first_name, last_name, year, phone_number);
+
+})
 // certing apis
 app.get('/', async (req, res) => {
     res.send("hello Shakil !!")
